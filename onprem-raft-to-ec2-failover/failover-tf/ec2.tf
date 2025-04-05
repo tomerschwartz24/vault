dbe128a893a42dfe991b822bca5c7c0c34d78827
Tomer <tomer.schwartz@zadarastorage.com>
2025-04-05 11:12:09 +0300

    seperate iam related stuff to iam.tf

--- onprem-raft-to-ec2-failover/failover-tf/ec2.tf
+++ onprem-raft-to-ec2-failover/failover-tf/ec2.tf
@@ -6,53 +6,9 @@ data "template_file" "user_data" {
     vault_ca      = file("${var.path_to_ca_crt}")
     vault_key     = file("${var.path_to_key}")
     api_addr      = var.vault_api_addr
-    vault_snapshots_bucket     = var.vault_snapshots_bucket  # Added the bucket name to user_data
+    vault_snapshots_bucket     = var.vault_snapshots_bucket
   }
 }
-
-resource "aws_iam_role" "ec2_s3_role" {
-  name = "ec2-s3-access-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "s3-access-policy"
-  description = "Policy to allow EC2 instance to access S3 buckets"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "s3:ListBucket"        # Added ListBucket permission
-        Effect   = "Allow"
-        Resource = "arn:aws:s3:::${var.vault_snapshots_bucket}"  # Allows listing the bucket itself
-      },
-      {
-        Action   = "s3:GetObject"         # Allows getting objects from the bucket
-        Effect   = "Allow"
-        Resource = "arn:aws:s3:::${var.vault_snapshots_bucket}/*"  # Allows access to all objects in the bucket
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_s3_policy_attach" {
-  role       = aws_iam_role.ec2_s3_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
 resource "aws_instance" "vault" {
   ami                    = var.ami
   instance_type          = var.instance_type
@@ -68,11 +24,6 @@ resource "aws_instance" "vault" {
   tags = {
     Name = var.instance_name
   }
-}
-
-resource "aws_iam_instance_profile" "ec2_s3_profile" {
-  name = "ec2-s3-instance-profile"
-  role = aws_iam_role.ec2_s3_role.name
 }
 
 resource "tls_private_key" "vault_key" {
---
+++ onprem-raft-to-ec2-failover/failover-tf/iam.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2-s3-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3-access-policy"
+  description = "Policy to allow EC2 instance to access S3 buckets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:ListBucket"
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${var.vault_snapshots_bucket}"
+      },
+      {
+        Action   = "s3:GetObject"        
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${var.vault_snapshots_bucket}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_policy_attach" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "ec2-s3-instance-profile"
+  role = aws_iam_role.ec2_s3_role.name
+}
